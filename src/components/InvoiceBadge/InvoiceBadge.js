b70e5ed25a25a08f158c0264716335670fe7667e
Joseph Lynn <josephlynn94@gmail.com>
2021-04-03 08:45:12 -0400

    added default status for badge component

--- src/components/InvoiceBadge/InvoiceBadge.js
+++ src/components/InvoiceBadge/InvoiceBadge.js
@@ -4,13 +4,21 @@ import './InvoiceBadge.scss';
 
 const STATUS = ['draft', 'pending', 'paid'];
 
+const getDefaultStatus = (arr) => {
+  return arr[0][0].toUpperCase() + arr[0].substring(1).toLowerCase();
+};
+
 const InvoiceBadge = ({ status }) => {
-  const checkBadgeStatus = STATUS.includes(status) ? status : STATUS[0];
+  const checkBadgeStatus = STATUS.includes(status)
+    ? status
+    : getDefaultStatus(STATUS);
 
   return (
     <span className={`badge ${checkBadgeStatus}`}>
       <span className={`badge__dot ${checkBadgeStatus}`}></span>
-      {status && status[0].toUpperCase() + status.substring(1).toLowerCase()}
+      {status
+        ? status[0].toUpperCase() + status.substring(1).toLowerCase()
+        : getDefaultStatus(STATUS)}
     </span>
   );
 };
--- src/components/InvoiceBadge/InvoiceBadge.test.js
+++ src/components/InvoiceBadge/InvoiceBadge.test.js
@@ -1,6 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import InvoiceBadge from './InvoiceBadge';
 
+it('should render a badge with a default status of draft', () => {
+  render(<InvoiceBadge />);
+
+  const badge = screen.getByText('Draft');
+
+  expect(badge).toBeInTheDocument();
+});
+
 it('should render a badge with a status of draft', () => {
   render(<InvoiceBadge status='draft' />);
 
