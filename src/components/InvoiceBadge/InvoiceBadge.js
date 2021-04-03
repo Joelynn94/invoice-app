fd6b5b3cd2b011ef85d2f79cd935b2dadc20182e
Joseph Lynn <josephlynn94@gmail.com>
2021-04-03 08:58:42 -0400

    changed badge classnames

--- src/components/InvoiceBadge/InvoiceBadge.js
+++ src/components/InvoiceBadge/InvoiceBadge.js
@@ -12,7 +12,7 @@ const InvoiceBadge = ({ status }) => {
   const checkBadgeStatus = STATUS.includes(status) ? status : STATUS[0];
 
   return (
-    <span className={`badge ${checkBadgeStatus}`}>
+    <span className={`badge badge-${checkBadgeStatus}`}>
       <span className={`badge__dot ${checkBadgeStatus}`}></span>
       {status
         ? status[0].toUpperCase() + status.substring(1).toLowerCase()
