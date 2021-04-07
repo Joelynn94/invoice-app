544efb870bdc8176d333369755cf73e3808eaf62
Joseph Lynn <josephlynn94@gmail.com>
2021-04-07 08:38:47 -0400

    fixed normalize overriding styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import ThemeContextProvider from './context/ThemeContext';
+import './app.global.scss';
+import './App.scss';
 import Layout from './components/Layout';
+import ThemeContextProvider from './context/ThemeContext';
 import Sidebar from './components/Sidebar/Sidebar';
 import Dashboard from './components/pages/Dashboard';
 import ViewInvoice from './components/pages/ViewInvoice';
-
-import './app.global.scss';
-import './App.scss';
 
 function App() {
   return (
