b03a61e55c3ab1d5a3c2af8cb6314483ce5a4cb7
Joseph Lynn <josephlynn94@gmail.com>
2021-04-14 07:05:51 -0400

    fixing type coercion issue with the price and total

--- src/components/BillItem/BillItem.js
+++ src/components/BillItem/BillItem.js
@@ -1,48 +1,56 @@
 import React, { useContext, useState } from 'react';
 import { ThemeContext } from '../../context/ThemeContext';
+import { AppContext } from '../../context/AppContext';
 import FormInput from '../FormInput/FormInput';
 import Heading from '../Heading/Heading';
 
 import './BillItem';
 
 const BillItem = () => {
+  const { dispatch } = useContext(AppContext);
   const { isLightTheme, light, dark } = useContext(ThemeContext);
   const theme = isLightTheme ? light : dark;
 
   const [item, setItem] = useState({
     itemName: '',
-    itemQty: '',
+    itemQty: 0,
     itemPrice: '',
     itemTotal: '',
   });
+
+  const toCurrency = (str) => {
+    const formatStrToCurrency = parseFloat(str).toFixed(2);
+    return formatStrToCurrency;
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     setItem({
-      itemName: '',
-      itemQty: '',
-      itemPrice: '',
-      itemTotal: '',
+      itemName: item.itemName,
+      itemQty: parseInt(item.itemQty),
+      itemPrice: parseFloat(item.itemPrice).toFixed(2),
+      itemTotal: parseFloat(item.itemTotal).toFixed(2),
     });
-  };
-
-  const toCurrency = (num) => {
-    return new Intl.NumberFormat().format(num);
   };
 
   const handleChange = (event) => {
     const { name, value, type } = event.target;
 
     console.log({
+      ...item,
       [name]:
-        type === 'number' ? Number.parseFloat(value).toFixed(2) : value.trim(),
+        name === 'itemPrice' || name === 'itemTotal'
+          ? parseFloat(value).toFixed(2)
+          : value.trim(),
     });
 
     setItem({
       ...item,
       [name]:
-        type === 'number' ? Number.parseFloat(value).toFixed(2) : value.trim(),
+        name === 'itemPrice' || name === 'itemTotal'
+          ? parseFloat(value).toFixed(2)
+          : value.trim(),
     });
   };
 
@@ -65,7 +73,7 @@ const BillItem = () => {
         name='itemQty'
         type='number'
         onChange={handleChange}
-        value={toCurrency(item.itemQty)}
+        value={item.itemQty}
         label='Qty'
         style={{
           backgroundColor: theme.cardBg,
@@ -77,7 +85,7 @@ const BillItem = () => {
         name='itemPrice'
         type='number'
         onChange={handleChange}
-        value={toCurrency(item.itemPrice)}
+        value={parseFloat(toCurrency(item.itemPrice))}
         label='Price'
         style={{
           backgroundColor: theme.cardBg,
@@ -89,7 +97,7 @@ const BillItem = () => {
         name='itemTotal'
         type='number'
         onChange={handleChange}
-        value={item.itemTotal}
+        value={parseFloat(toCurrency(item.itemTotal))}
         label='Total'
         style={{
           backgroundColor: theme.cardBg,
