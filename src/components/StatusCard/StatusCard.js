a939601f451ac299e1710e1a920956fdb724c507
Joseph Lynn <josephlynn94@gmail.com>
2021-08-17 08:20:21 -0400

    ADDED: ability to change an invoice status to paid and delete an invoice

--- src/components/StatusCard/StatusCard.js
+++ src/components/StatusCard/StatusCard.js
@@ -1,18 +1,20 @@
-import React, { useContext } from 'react';
-import { ThemeContext } from '../../context/ThemeContext';
-import InvoiceBadge from '../InvoiceBadge/InvoiceBadge';
-import InvoiceViewButtons from '../InvoiceViewButtons/InvoiceViewButtons';
+import React, { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
+import InvoiceBadge from "../InvoiceBadge/InvoiceBadge";
+import InvoiceViewButtons from "../InvoiceViewButtons/InvoiceViewButtons";
 
-import './StatusCard.scss';
+import "./StatusCard.scss";
 
 const StatusCard = ({ invoice }) => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
   const theme = isLightTheme ? light : dark;
+
   return (
-    <div className='status-card' style={{ backgroundColor: theme.cardBg }}>
-      <p className='status-card__status-text'>Status</p>
+    <div className="status-card" style={{ backgroundColor: theme.cardBg }}>
+      <p className="status-card__status-text">Status</p>
       <InvoiceBadge status={invoice.status} />
       <InvoiceViewButtons
+        invoice={invoice}
         style={{
           backgroundColor: theme.cardBg,
           color: theme.text,
--- src/pages/Invoice/Invoice.js
+++ src/pages/Invoice/Invoice.js
@@ -1,21 +1,21 @@
-import React, { useContext, useEffect } from 'react';
-import { AppContext } from '../../context/AppContext';
-import GoBack from '../../components/GoBack/GoBack';
-import StatusCard from '../../components/StatusCard/StatusCard';
-import InvoiceDetails from '../../components/InvoiceDetails/InvoiceDetails';
+import React, { useContext, useEffect } from "react";
+import { AppContext } from "../../context/AppContext";
+import GoBack from "../../components/GoBack/GoBack";
+import StatusCard from "../../components/StatusCard/StatusCard";
+import InvoiceDetails from "../../components/InvoiceDetails/InvoiceDetails";
 
-import './Invoice.scss';
+import "./Invoice.scss";
 
 const Invoice = ({ match }) => {
-  const { invoices, getInvoices } = useContext(AppContext);
+  const { invoices, getInvoice } = useContext(AppContext);
   const invoice = invoices.find((invoice) => invoice.id === match.params.id);
 
   useEffect(() => {
-    getInvoices(invoices);
-  }, [invoices]);
+    getInvoice(invoice);
+  }, [invoice]);
 
   return (
-    <main className='invoice'>
+    <main className="invoice">
       <GoBack />
       <StatusCard invoice={invoice} />
       <InvoiceDetails invoice={invoice} />
